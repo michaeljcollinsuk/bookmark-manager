08eae8a48e058e8ac05203eeb55eff95dcdf5ae1
innlouvate <l.j.franklin85@googlemail.com>
2016-02-09 12:28:44 +0000

    display links on page

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,15 @@
 require 'sinatra/base'
+require_relative 'models/link.rb'
 
 class BookmarkManager < Sinatra::Base
+
   get '/' do
     'Hello BookmarkManager!'
+  end
+
+  get '/links' do
+    @links = Link.all
+    erb (:links)
   end
 
   # start the server if ruby file executed directly
