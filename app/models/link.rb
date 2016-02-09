567ab3170e477c9475b28b10ea4f61e7028dd15e
innlouvate <l.j.franklin85@googlemail.com>
2016-02-09 17:06:12 +0000

    move db link

--- app/models/link.rb
+++ app/models/link.rb
@@ -13,3 +13,7 @@ end
 # DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 # DataMapper.finalize
 # DataMapper.auto_upgrade!
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.finalize
+DataMapper.auto_upgrade!
