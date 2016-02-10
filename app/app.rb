5c3796a69e73b4f997c00cace162170cfa4e3c99
Michael Collins <michaelj.collinsuk@gmail.com>
2016-02-10 17:46:10 +0000

    Using rake

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,8 @@
+ENV['RACK_ENV'] ||= 'development'
 require 'sinatra/base'
 require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
-
-  ENV['RACK_ENV'] ||= 'development'
 
   get '/' do
     redirect '/links'
