f45e0094ab8b9b4d1aabeefa446772f5414f277e
Michael Collins <michaelj.collinsuk@gmail.com>
2016-02-12 10:19:57 +0000

    Failing tests

--- spec/features/add_users_spec.rb
+++ spec/features/add_users_spec.rb
@@ -2,6 +2,10 @@ feature 'adding users' do
   scenario 'user signs up' do
     expect{sign_up}.to change(User, :count).by(1)
     expect(page).to have_content 'Welcome, michael@email.com'
-    # expect(User)
+    expect(User.first.email).to eq('michael@email.com')
+  end
+
+  scenario 'user incorrectly confirms password' do
+    expect{bad_sign_up}.to change(User, :count).by(0)
   end
 end
--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -1,6 +1,15 @@
 def sign_up
   visit '/users/new'
   fill_in 'email', with: 'michael@email.com'
-  # fill_in 'password', with: 'p4ssword'
+  fill_in 'password', with: 'p4ssword'
+  fill_in 'confirm_password', with: 'p4ssword'
+  click_button 'Sign up'
+end
+
+def bad_sign_up
+  visit '/users/new'
+  fill_in 'email', with: 'michael@email.com'
+  fill_in 'password', with: 'p4ssword'
+  fill_in 'confirm_password', with: 'p8ssword'
   click_button 'Sign up'
 end
